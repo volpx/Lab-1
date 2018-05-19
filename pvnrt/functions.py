36659d2cedc246bb6f11d204dc5ce4ba4b228b20
Filippo Volpe <filippovolpe98@gmail.com>
2018-05-19 23:42:27 +0200

    started analysis, liner regression,chi_ht,p_m,zeroguess

---
+++ pvnrt/functions.py
@@ -0,0 +1,35 @@
+## Library of functions
+import numpy as np
+import pdb
+
+def w_mean(data,weigth=None):
+    if weigth.any():
+        mean=np.sum(data*weigth)/np.sum(weigth)
+        dm=np.sqrt(1/np.sum(weigth))
+    else:
+        mean=np.mean(data)
+        dm=np.sqrt(1/len(data))*np.std(data)
+    return mean,dm
+
+def chi2(data,dy,model):
+    return np.sum(((model - data)**2)/(dy**2))
+
+def linear_regression_AB(x,y,w):
+    w=w*np.ones(len(x))
+    #pdb.set_trace()
+    dw=np.sum(w) * \
+            np.sum(w*(x**2)) - \
+            (np.sum(w*x))**2
+    A=( np.sum(w*(x**2))* \
+        np.sum(w*y) - \
+        np.sum(w*x) * \
+        np.sum(w*x*y) ) / \
+        dw
+    B=( np.sum(w) * \
+        np.sum(w*x*y) - \
+        np.sum(w*y) * \
+        np.sum(w*x) ) / \
+        dw
+    dA= np.sqrt(np.sum(((x)**2)*w) / dw)
+    dB= np.sqrt(np.sum(w)/dw)
+    return A,B,dA,dB
---
+++ pvnrt/main_pvnrt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from pvnrt_1 import parte1
+def main():
+    parte1()
+
+if __name__ == '__main__':
+    main()
