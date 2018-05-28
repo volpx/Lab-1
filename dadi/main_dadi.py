2f5716b94be21587b0d9ea115f2487da1f2a716d
Filippo Volpe <filippovolpe98@gmail.com>
2018-05-28 18:49:10 +0200

    rf

--- dadi/main_dadi.py
+++ dadi/main_dadi.py
@@ -8,14 +8,23 @@ from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 
 from functions import *
 
+def trow_6():
+    return int(np.random.rand()*6)+1
+
 def trow_12():
     return int(np.random.rand()*12)+1
 
 def trow_20():
     return int(np.random.rand()*20)+1
 
-def main():
+def trow_n_time(n,trow):
+    out=np.empty(n,dtype=np.int8)
+    for i in range(n):
+        out[i]=trow()
+    return out
 
+def main():
+    print(trow_n_time(100,trow_6))
 
 if __name__ == '__main__':
     main()
