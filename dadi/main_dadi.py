30def815d4042baac9749127c7db7bdecd3ec5c4
Filippo Volpe <filippovolpe98@gmail.com>
2018-05-28 14:06:57 +0200

    added dadi

---
+++ dadi/main_dadi.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import numpy as np
+from uncertainties import ufloat
+import csv
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
+
+from functions import *
+
+def trow_12():
+    return int(np.random.rand()*12)+1
+
+def trow_20():
+    return int(np.random.rand()*20)+1
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
